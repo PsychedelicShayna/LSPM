885e218050965601eba76e495e5d41f0d50752a7
Shayna Kaine <Fedorable@protonmail.ch>
2019-07-01 05:32:54 -0300

    Removed branch test comment

--- source/main.cxx
+++ source/main.cxx
@@ -5,9 +5,6 @@
 
 #include "headers/main_window_dialogue.hxx"
 
-/*
- * This comment should only appear on the development branch. */
-
 int main(int argc, char** argv) {
     QApplication application(argc, argv);
 
