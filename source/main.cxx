02ee90cd31b616e2cb2461089bb67e54cbb71786
Shayna Kaine <Fedorable@protonmail.ch>
2019-07-01 05:32:16 -0300

    Branch test.

--- source/main.cxx
+++ source/main.cxx
@@ -5,6 +5,9 @@
 
 #include "headers/main_window_dialogue.hxx"
 
+/*
+ * This comment should only appear on the development branch. */
+
 int main(int argc, char** argv) {
     QApplication application(argc, argv);
 
